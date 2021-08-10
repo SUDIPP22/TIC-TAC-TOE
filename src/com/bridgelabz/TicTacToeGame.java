cf760e54e98586170cba9a3c596a862ea677dfa5
SUDIPP22 <panja370@gmail.com>
2021-08-10 10:17:12 +0530

    Modified : Creating an empty board

--- src/com/bridgelabz/TicTacToeGame.java
+++ src/com/bridgelabz/TicTacToeGame.java
@@ -3,12 +3,15 @@ package com.bridgelabz;
 public class TicTacToeGame {
     public static void main(String[] args) {
         System.out.println("Welcome To TIC-TAC-TOE Game");
-        getBoard();   // calling method
+        char[] board= getBoard();   // calling method
     }
-        public static void getBoard() {
+
+    public static char[] getBoard() {
         char[] board = new char[10];
-        for (int i=1;i<= board.length;i++) {
-            board[i] = ' ';
+        // loop execution
+        for (int index = 1; index < board.length; index++) {
+            board[index] = ' ';       // assigning empty space
         }
+        return board;
     }
 }
