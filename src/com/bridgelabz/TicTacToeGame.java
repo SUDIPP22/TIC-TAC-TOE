eb42ce538df89e6d7c239430c34c323ddeb13a31
SUDIPP22 <panja370@gmail.com>
2021-08-11 01:56:57 +0530

    Refactored : Creation Of An Empty Board

--- src/com/bridgelabz/TicTacToeGame.java
+++ src/com/bridgelabz/TicTacToeGame.java
@@ -1,16 +1,35 @@
 package com.bridgelabz;
-
+/**
+ * Purpose : To create a Tic Tac Toe Game Board
+ * @author SUDIP PANJA
+ * @version 11.0.11
+ * @since 2021-08-10
+ * */
 public class TicTacToeGame {
+    public static char[] board;
+
+    /**
+     * This is the main method which makes use of createBoard method
+     *
+     * @param args Unused
+     */
     public static void main(String[] args) {
         System.out.println("Welcome To TIC-TAC-TOE Game");
-        char[] board= getBoard();   // calling method
+        board = createBoard();
     }
 
-    public static char[] getBoard() {
-        char[] board = new char[10];
+    /**
+     * This method implements to create a TicTacToe board
+     *
+     * @return board This returns the TicTacToe board
+     * @index This is used for initializing the index for iteration from 1 to 9
+     * and stores the empty space
+     */
+    public static char[] createBoard() {
+        board = new char[10];
         // loop execution
         for (int index = 1; index < board.length; index++) {
-            board[index] = ' ';       // assigning empty space
+            board[index] = ' ';
         }
         return board;
     }
