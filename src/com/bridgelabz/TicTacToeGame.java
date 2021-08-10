b30e349ef981d5c3997c13335cc92c17b8f72c4b
SUDIPP22 <panja370@gmail.com>
2021-08-10 23:17:19 +0530

    Modified : Welcome Messsage

--- src/com/bridgelabz/TicTacToeGame.java
+++ src/com/bridgelabz/TicTacToeGame.java
@@ -1,17 +1,12 @@
 package com.bridgelabz;
-
+/**
+ * Purpose : To create a Tic Tac Toe Game
+ * @author SUDIP PANJA
+ * @version 11.0.11
+ * @since 2021-08-10
+ * */
 public class TicTacToeGame {
     public static void main(String[] args) {
         System.out.println("Welcome To TIC-TAC-TOE Game");
-        char[] board= getBoard();   // calling method
-    }
-
-    public static char[] getBoard() {
-        char[] board = new char[10];
-        // loop execution
-        for (int index = 1; index < board.length; index++) {
-            board[index] = ' ';       // assigning empty space
-        }
-        return board;
     }
 }
