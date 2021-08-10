865d6db63be50dab0f3994ea5363d1448b6fcea7
SUDIPP22 <panja370@gmail.com>
2021-08-10 08:58:42 +0530

    Refactored : Creating a board

--- src/com/bridgelabz/TicTacToeGame.java
+++ src/com/bridgelabz/TicTacToeGame.java
@@ -3,5 +3,12 @@ package com.bridgelabz;
 public class TicTacToeGame {
     public static void main(String[] args) {
         System.out.println("Welcome To TIC-TAC-TOE Game");
+        getBoard();   // calling method
+    }
+        public static void getBoard() {
+        char[] board = new char[10];
+        for (int i=1;i<= board.length;i++) {
+            board[i] = ' ';
+        }
     }
 }
