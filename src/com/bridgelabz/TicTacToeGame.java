f6df2722549877a4247f2206ad06764496b5b968
SUDIPP22 <panja370@gmail.com>
2021-08-23 04:24:33 +0530

    Refactored : Checking who will play first? user or computer.

--- src/com/bridgelabz/TicTacToeGame.java
+++ src/com/bridgelabz/TicTacToeGame.java
@@ -1,5 +1,6 @@
 package com.bridgelabz;
 
+import java.util.Random;
 import java.util.Scanner;
 
 /**
@@ -14,31 +15,35 @@ public class TicTacToeGame {
     public static char playerChoice;
     public static char computerChoice;
     public static Scanner sc = new Scanner(System.in);
+    public static Random random = new Random();
 
     /**
      * This is the main method which makes use of createBoard method,
-     * playerTurn method and
-     * showBoard method
-     *
+     * showBoard method, chooseOption method, checkFirstPlayer method and
+     * isSpaceFree method.
      * @param args Unused
      */
     public static void main(String[] args) {
         System.out.println("Welcome To TIC-TAC-TOE Game");
         board = createBoard();
-        playerTurn(sc);
         showBoard();
-        userPlay(sc);
+        System.out.println("Choose An Option between X or O ");
+        char choice = sc.next().charAt(0);
+        chooseOption(choice);
+        checkFirstPlayer();
+
+        //userPlay(sc);
         /*
         * This condition indicates if free space  is available on the board
         * then user will be making desired move
          */
         if (isSpaceFree(board, 9))
             userPlay(sc);
+        sc.close();
     }
 
     /**
      * This method implements to create a TicTacToe board
-     *
      * @return board This returns the TicTacToe board
      * @index This is used for initializing the index for iteration from 1 to 9
      * and stores the empty space
@@ -53,19 +58,18 @@ public class TicTacToeGame {
 
     /**
      * This method implements player choose between X or O
-     * @param sc This is the first parameter to playerTurn method
+     * @return player choice
      */
-    public static void playerTurn(Scanner sc) {
-        System.out.println("CHOOSE AN OPTION X or O ");
-        char option = sc.next().charAt(0);
-        if (option == 'X') {
+    public static char chooseOption(char choice) {
+        if (choice == 'X') {
             playerChoice = 'X';
             computerChoice = 'O';
-        } else if (option == 'O') {
+        } else if (choice == 'O') {
             computerChoice = 'X';
             playerChoice = 'O';
         } else
             System.out.println("PLEASE ENTER A VALID OPTION");
+        return playerChoice;
     }
 
     /**
@@ -122,6 +126,46 @@ public class TicTacToeGame {
     }
 
     /**
+     * This computerPlay method indicates the ability for computer to
+     * make a move randomly in the board
+     */
+    public static void computerPlay() {
+        int computerPlay = random.nextInt(9) + 1;
+        if (board[computerPlay] == ' ') {
+            switch (computerPlay) {
+                case 1:
+                    board[1] = computerChoice;
+                    break;
+                case 2:
+                    board[2] = computerChoice;
+                    break;
+                case 3:
+                    board[3] = computerChoice;
+                    break;
+                case 4:
+                    board[4] = computerChoice;
+                    break;
+                case 5:
+                    board[5] = computerChoice;
+                    break;
+                case 6:
+                    board[6] = computerChoice;
+                    break;
+                case 7:
+                    board[7] = computerChoice;
+                    break;
+                case 8:
+                    board[8] = computerChoice;
+                    break;
+                case 9:
+                    board[9] = computerChoice;
+                    break;
+            }
+        }
+        showBoard();
+    }
+
+    /**
      * This isSpaceFree method indicates ability for checking the free space on the board
      * @param board This is the first parameter to isSpaceFree method
      * @param index This is the second parameter to isSpaceFree method
@@ -129,6 +173,24 @@ public class TicTacToeGame {
      */
     public static boolean isSpaceFree(char[] board, int index) {
         return board[index] == ' ';
+    }
+
+    /**
+     * This method is used to determine who will play first
+     */
+    public static void checkFirstPlayer() {
+        int user = random.nextInt(2);
+        int computer = random.nextInt(2);
+        if (user == 0) {
+            System.out.println("User Got The First Chance");
+            userPlay(sc);
+            System.out.println("Now The Board is : ");
+            computerPlay();
+        } else if (computer == 0) {
+            System.out.println("Computer Got The First Chance");
+            computerPlay();
+            userPlay(sc);
+        }
     }
 }
 
